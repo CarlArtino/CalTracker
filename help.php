3304ba18220ffb40ff6d67494166958587e214fe
CarlArtino <cma119@zips.uakron.edu>
2020-11-29 21:42:55 -0500

    Add logo

--- help.php
+++ help.php
@@ -11,7 +11,9 @@
     </head>
     <body>
       <nav class="navbar sticky-top navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
-        <a class="navbar-brand" href="#">CalTracker</a>
+        <a class="navbar-brand" href="#">
+			<img src="pictures/logo.png" alt="logo">
+		</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
